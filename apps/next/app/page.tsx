36a0741cc75ee3cd011433a48264f87bd81ca743
S-P-H-E <sphe.g.personal@gmail.com>
2026-01-02 22:48:43 +0200

    Update homepage layout and image styling
    - Adjusted the height of product cards on the homepage for better visual consistency.
    - Enhanced image styling to ensure proper aspect ratio and object fitting, improving overall presentation.

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -8,8 +8,8 @@ export default async function Home() {
     <div className='flex gap-6 h-dvh items-center justify-center'>
       {data && data.length > 0 ? (
         data.map(d => (
-          <div key={d.id} className='w-xs h-[450px] border border-black shadow-2xl cursor-pointer transition-all duration-500 hover:scale-105 p-4 rounded-2xl flex flex-col gap-2'>
-            <Image src={d.assets[0].url} className='rounded-xl' alt="Image" width={400} height={0} />
+          <div key={d.id} className='w-xs h-[420px] border border-black shadow-2xl cursor-pointer transition-all duration-500 hover:scale-105 p-4 rounded-2xl flex flex-col gap-2'>
+            <Image src={d.assets[0].url} className='rounded-xl w-full h-[260px] object-cover' alt="Image" width={400} height={0} />
             <h1 className='font-semibold text-3xl'>{d.title}</h1>
             <p>{d.description}</p>
             <p className='font-semibold'>{d.price}</p>
